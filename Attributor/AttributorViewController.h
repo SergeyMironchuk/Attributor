715eda2bde56bc13925a4abd82ef5f2c4eedfc75
Prn Service <admin@prn-service.com>
2015-10-16 16:00:41 +0300

    Первоначальная загрузка

---
+++ Attributor/AttributorViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Admin on 15.10.15.
+//  Copyright (c) 2015 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributorViewController : UIViewController
+
+
+@end
+
